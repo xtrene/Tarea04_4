eebf502e83d3057f61d324c6b4e77a841445d319
Carlos <Carlos@xtrene.com>
2021-06-21 18:41:25 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tarea04_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un número: ");
+            int num1 = Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Escribe otro número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2) {
+                Console.WriteLine("{0} es mayor que {1}", num1, num2);
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Los dos números son iguales");
+            }
+            else
+
+            {
+                Console.WriteLine("{0} es mayor que {1}", num2, num1);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
